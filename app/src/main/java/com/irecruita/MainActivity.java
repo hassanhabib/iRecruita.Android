d5ab1878e888da2080ffd09d0bd1a9527af6b433
hassan <hassanhabib@live.com>
2016-01-10 00:13:07 -0600

    Initializing

---
+++ app/src/main/java/com/irecruita/MainActivity.java
@@ -0,0 +1,17 @@
+package com.irecruita;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView web = (WebView) findViewById(R.id.webView);
+        web.loadUrl("http://www.irecruita.com");
+    }
+}
